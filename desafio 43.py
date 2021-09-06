45d2f3c1919b832736348ab7d0551af4894b5baa
RegianeAlves <regianealves2410@gmail.com>
2021-09-06 20:47:51 -0300

    desafio 43

--- desafio 43.py
+++ desafio 43.py
@@ -1,1 +1,13 @@
-gfgfg
+altura = float(input('Digite sua altura: '))
+peso = float(input('Digite seu peso: '))
+imc = peso / (altura * altura)
+if imc < 18.5:
+    print('Seu IMC é: {:.2f}, Você está abaixo do peso!'.format(imc))
+elif imc >= 18.5 and imc < 25:
+    print('Seu IMC é: {:.2f}, Você está com o peso ideal!'.format(imc))
+elif imc >= 25 and imc < 30:
+    print('Seu IMC é: {:.2f}, Você está com obesidade!'.format(imc))
+elif imc >= 30 and imc <= 40:
+    print('Seu IMC é: {:.2f}, Você está com sobrepeso!'.format(imc))
+else:
+    print('Seu IMC é: {:.2f}, Você está com obesidade mórbida!'.format(imc))
