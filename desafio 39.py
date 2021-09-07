a724228fa367159c8673d61366adeec65de4500b
RegianeAlves <regianealves2410@gmail.com>
2021-09-07 18:36:25 -0300

    desafio 39

--- desafio 39.py
+++ desafio 39.py
@@ -4,10 +4,11 @@ nascimento = int(input('Digite o ano de nascimento: '))
 ano = date.today().year
 idade = ano - nascimento
 s = 18 - idade
-s2 = idade - 18
+
 if idade < 18:
     print('Você tem {} anos, ainda falta {} anos para se alistar'.format(idade, s))
 elif idade == 18:
     print('Você tem {} anos, está na hora de se alistar'.format(idade))
 else:
+    s2 = idade - 18
     print('Você tem {} anos, já passou {} anos do tempo de se alistar'.format(idade, s2))
