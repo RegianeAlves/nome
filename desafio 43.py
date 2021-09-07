10e3bc93df91d863bd21a64c4d0fe835293e6dc4
RegianeAlves <regianealves2410@gmail.com>
2021-09-07 18:53:39 -0300

    att desafio 43 IMC

--- desafio 43.py
+++ desafio 43.py
@@ -7,7 +7,7 @@ elif imc >= 18.5 and imc < 25:
     print('Seu IMC é: {:.2f}, Você está com o peso ideal!'.format(imc))
 elif imc >= 25 and imc < 30:
     print('Seu IMC é: {:.2f}, Você está com obesidade!'.format(imc))
-elif imc >= 30 and imc <= 40:
+elif imc >= 30 and imc < 40:
     print('Seu IMC é: {:.2f}, Você está com sobrepeso!'.format(imc))
 else:
     print('Seu IMC é: {:.2f}, Você está com obesidade mórbida!'.format(imc))
