8f547d1f502a490df6f179cb220fa35bce90ae4a
RegianeAlves <regianealves2410@gmail.com>
2021-09-23 18:37:42 -0300

    att desafio 64 Somando valores / contador

--- desafio64.py
+++ desafio64.py
@@ -3,8 +3,8 @@ n = 0
 num = 0
 while n != 999:
     num = num + n
-    n = int(input('Digite um número: '))
+    n = int(input('Digite um número: (Digite [999] para parar!) '))
     contador += 1
-print(num)
+print('A soma desses números é: {}'.format(num))
 print('Números digitados {}'.format(contador - 1))
-print('Tchau')
+print('Tchau!')
