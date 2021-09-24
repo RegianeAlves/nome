b7dc7a5874b87bd8cd0edcab5857a4fe57c5c54d
RegianeAlves <regianealves2410@gmail.com>
2021-09-24 20:13:14 -0300

    desafio 68 while / break par ou impar

---
+++ desafio68.py
@@ -0,0 +1,26 @@
+from random import randint
+
+contador = 0
+while True:
+    aleatorio = randint(0, 10)
+    n = int(input('Diga um valor: '))
+    n2 = str(input('Par ou Impar [P/I]: ')).strip().upper()[0]
+    soma = aleatorio + n
+    if soma % 2 == 0:
+        print(f'O computador pensou em {aleatorio} e você jogou {n}. A Soma deu {soma} deu Par')
+        if n2 == 'P':
+            print('Parabens você venceu!')
+            contador += 1
+        else:
+            print('Você perdeu!')
+            break
+    else:
+        print(f'O computador pensou em {aleatorio} e você jogou {n}. A Soma deu {soma} deu Impar')
+        if n2 == 'I':
+            print('Parabens você venceu!')
+            contador += 1
+        else:
+            print('Você perdeu!')
+            break
+
+print(f'Você venceu {contador} vezes')
