af1493f2d84a3163842f8aecbb13cd8eb3589e0d
RegianeAlves <regianealves2410@gmail.com>
2021-11-06 17:36:52 -0300

    desafio 96 area de um terreno com função

---
+++ desafio96.py
@@ -0,0 +1,7 @@
+def area(a, b):
+    print(f'A área de um terreno {a}x{b} é de {a*b}m²')
+
+
+largura = float(input('LARGURA (m): '))
+comprimento = float(input('COMPRIMENTO (m): '))
+area(largura, comprimento)
