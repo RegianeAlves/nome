f043492ba6d71655f59ee18ca3607b4213d06814
RegianeAlves <regianealves2410@gmail.com>
2021-11-06 17:59:41 -0300

    desafio 99 funçao que descobre o maior

---
+++ desafio99.py
@@ -0,0 +1,30 @@
+from time import sleep
+
+
+def maior(*num):
+    cont = 0
+    print('Analizando os valores passados...')
+    for valor in num:
+        print(f'{valor}', end=' ')
+        cont += 1
+    print(f'foram informados {cont} valores ao todo')
+    if cont == 0:
+        print(f'O maior valor informado foi 0.')
+    else:
+        print(f'O maior valor informado foi {max(num)}.')
+    prt()
+    sleep(1)
+
+
+def prt():
+    print('-=' * 25)
+
+
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
+
+
+
